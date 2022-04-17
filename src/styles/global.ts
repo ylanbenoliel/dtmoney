884c3090eccbb005eb4cbb153410a2632bad2df2
Ylan Benoliel <ylanbenoliel@gmail.com>
2022-04-17 19:31:54 -0300

    Font definition

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -26,6 +26,15 @@ body {
     -webkit-font-smoothing: antialiased;
 }
 
+body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+} 
+
+h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+}
+
 html {
     @media (max-width: 1800px) {
         font-size: 93.75%;
