cd36473bd082f57853a68aa4f5e7ed94b05fc235
Ylan Benoliel <ylanbenoliel@gmail.com>
2022-04-17 20:44:16 -0300

    Created skeleton of Dashboard component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import * as S from "./styles";
+import { Summary } from "../Summary/index";
+
+export function Dashboard() {
+  return (
+    <S.Container>
+      <Summary />
+    </S.Container>
+  );
+}
