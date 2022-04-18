6ff84fae5bb96f8d2f89197f70ebc5f1294c7841
Ylan Benoliel <ylanbenoliel@gmail.com>
2022-04-17 21:53:20 -0300

    Added TransactionsTable to Dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import * as S from "./styles";
 import { Summary } from "../Summary/index";
+import { TransactionsTable } from "../TransactionsTable";
 
 export function Dashboard() {
   return (
     <S.Container>
       <Summary />
+      <TransactionsTable />
     </S.Container>
   );
 }
